eb84ea3e3fad7e049ec3191821cdcc0ecb7f315a
Andres Jaanson <A_Jaanson@hotmail.com>
2018-09-06 14:55:12 +0200

    Create initial user locations for testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_location (user_id, longitude, latitude) VALUES
+  ('Ermi', 12.56456, 87.77678),
+  ('Iiris', 32, 45.534),
+  ('Tiiu', 32.543, 23.765),
+  ('Olvi', 12.788, 12.434),
+  ('Killu', 24.4324, 98.4324);
