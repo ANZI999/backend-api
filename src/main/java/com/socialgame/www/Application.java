93409cb627025930b5f422ad36b226e1cc85b9b0
Andres Jaanson <A_Jaanson@hotmail.com>
2018-09-06 02:52:42 +0200

    Fix project so that it would run

--- src/main/java/com/socialgame/www/Application.java
+++ src/main/java/com/socialgame/www/Application.java
@@ -1,14 +1,9 @@
 package com.socialgame.www;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EntityScan(basePackages = { "com.socialgame.www" })
 public class Application {
 	
     public static void main(String[] args) {
--- src/main/java/com/socialgame/www/location/UserLocationRepositoryImpl.java
+++ src/main/java/com/socialgame/www/location/UserLocationRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.socialgame.www.location;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
